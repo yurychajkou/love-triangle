9718f1a68f3c8dd8cb1ca2b44cf87106e6b5468b
Aliaksandr Yankouski <aleksandr.yankovskiy@gmail.com>
2018-02-20 17:23:29 +0300

    Number of love triangles: counter added, Math trunc() truncation for results to avoid fractional output.

--- src/index.js
+++ src/index.js
@@ -4,4 +4,14 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+  var arr = preferences;
+  var sumAngle = 0;
+    for (var i =0; i <= arr.length-1; i++) {
+        var firstAngel = arr[i] -1;
+    var secAngel = arr[firstAngel] -1;
+    var thirdAngel = arr[secAngel] -1;
+        if (thirdAngel == i) {
+        sumAngle++}
+        }
+        return(Math.trunc(sumAngle/3))
+  } 
